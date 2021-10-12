546c7b673bfdf4abafccb1653ae0e7d60437c565
Dev07 <gunwoo07@github.com>
2021-10-12 13:07:48 +0900

    set view engine to ejs

--- 2021/index.js
+++ 2021/index.js
@@ -4,6 +4,8 @@ const app = express();
 
 // Import cookie-parser to read cookie from client
 const cookieParser = require("cookie-parser");
+// Import ejs to load html
+const ejs = require("ejs");
 
 // Import questionViewer
 const questionViewer = require("./src/questionViewer");
@@ -15,6 +17,8 @@ const port = 3000;
 
 // Declare that we are going to use cookie-parser
 app.use(cookieParser());
+// Declare view engine to ejs
+app.set("view engine", "ejs");
 // Declare routers
 app.use("/question", questionViewer);
 
