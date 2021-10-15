dd311f83ed08e1a280159923ec6375caf554b222
goorm <goorm@github.com>
2021-10-15 05:51:04 +0000

    middle

--- 2021/src/description.js
+++ 2021/src/description.js
@@ -3,7 +3,16 @@ var express = require("express");
 var router = express.Router();
 
 
+
+
 router.get("/", (req, res) => {
+	var cookies = req.cookies;
+	var nameOfCookies = Object.keys(cookies);
+	
+	for (var i = 0; i < nameOfCookies.length; i++) {
+		res.clearCookie(nameOfCookies[i]);
+		// res.clearCookie(nameOfCookies[i]);
+	}
     res.render("description");
 });
 
